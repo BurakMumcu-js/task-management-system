fb9b1a653107e4934989334d5f2e46d96a48018d
Burak Mumcu <95939910+BurakMumcu-js@users.noreply.github.com>
2023-11-11 11:35:29 +0300

    feat: gereklilikler yüklendi;feat: home login ve register componentleri için router yapısı oluşturuldu

---
+++ ui/src/pages/login.js
@@ -0,0 +1,9 @@
+import {Link, useNavigate} from "react-router-dom";
+import {useState} from "react";
+import axios from "axios";
+
+const LoginComponent = () => {
+
+}
+
+export default LoginComponent;
---
+++ ui/src/pages/register.js
@@ -0,0 +1,9 @@
+import {Link, useNavigate} from "react-router-dom";
+import {useState} from "react";
+import axios from "axios";
+
+const RegisterComponent = () => {
+
+}
+
+export default RegisterComponent;
