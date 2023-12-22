9cf85251d1fbbc741c260260b51eacd768693899
Burak Mumcu <95939910+BurakMumcu-js@users.noreply.github.com>
2023-12-22 14:50:45 +0300

    fix:cors hatası düzeltildi

--- dev/app.js
+++ dev/app.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const app = express();
-const http = require('http');
-const cors = require('cors')
-const corsOptions = require('./src/config');
-
 const {mainErrorHandler} = require('./src/middlewares/error.middleware')
 const registerRoutes = require('./src/routes/register.router')
 const loginRoutes = require('./src/routes/login.router');
 const channelsRoutes = require('./src/routes/channels.router')
 const tasksRoutes = require('./src/routes/tasks.router');
 const passwordRoutes = require('./src/routes/password.router');
-
+const config = require('./src/config');
+const cors = require("cors");
+const corsOptions = require("./src/config/cors");
 require('dotenv').config({path: 'src/.env'});
 app.use(express.json());
 app.use('/password',passwordRoutes)
@@ -19,9 +17,8 @@ app.use('/channel',channelsRoutes);
 app.use('/auth',registerRoutes)
 app.use('/auth',loginRoutes);
 app.use('/task',tasksRoutes);
-
 app.use(mainErrorHandler)
-
+config();
 const uri = process.env.MongoUri;
 mongoose.connect(uri).then(res => {
     console.log('db connected')
