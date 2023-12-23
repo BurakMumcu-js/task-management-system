3edd27dc24b10329fb1e0f2aa3fdcbb4830e8b34
Burak Mumcu <95939910+BurakMumcu-js@users.noreply.github.com>
2023-12-23 14:45:20 +0300

    feat:mail servislerinde nodemailer yerine sendgrid kullanılmına başlandı

--- dev/src/controllers/password.controller.js
+++ dev/src/controllers/password.controller.js
@@ -1,46 +1,34 @@
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 const { User } = require('../models/user.model')
 const crypto = require('crypto');
 const passwordChange = async (req,res) => {
     try {
-        const {email} = req.body;
+        const { email } = req.body;
         const user = await User.findOne({ email });
-// todo: error: Invalid login: 535-5.7.8 Username and Password not accepted. Learn more at\n535 5.7.8  https://support.google.com/mail/?p=BadCredentials k26-20020a170906681a00b009dd678d7d3fsm2828427ejr.211 - gsmtp
-        // todo: üstteki hata mesajı alınıyor bakılacak
+
         if (!user) {
             return res.status(404).json({ message: 'Kullanıcı bulunamadı.' });
         }
+
         const resetToken = crypto.randomBytes(20).toString('hex');
         user.resetPasswordToken = resetToken;
         user.resetPasswordExpires = Date.now() + 3600000; // 1 saat boyunca kullanılabilir
         await user.save();
-        const transporter = nodemailer.createTransport({
-            service: 'Gmail',
-            auth: {
-                user: 'userGmail',
-                pass: 'userPassword'
-            }
-        });
 
-        const mailOptions = {
-            from: 'yusuf.gurcan.21@gmail.com',
+        const msg = {
             to: email,
+            from: 'from@mail.com',
             subject: 'Parola Sıfırlama',
-            text: 'Parolanız sıfırlandı: Yeni Parolanız: 123456'
+            text: 'Parolanız sıfırlandı: Yeni Parolanız: 123456',
         };
-        await transporter.sendMail(mailOptions);
 
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return console.log(error);
-            }
-           console.log('E-posta gönderildi: ' + info.response);
-        });
+        await sgMail.send(msg);
 
-
-
+        console.log('E-posta gönderildi.');
+        res.status(200).json({ message: 'Şifre sıfırlama e-postası gönderildi.' });
     } catch (e) {
-        res.status(500).json({message:e.message});
+        console.error(e);
+        res.status(500).json({ message: e.message });
     }
 }
 
