2a021fbeba835c154f4d2ddcf2ed716e245624af
Burak Mumcu <95939910+BurakMumcu-js@users.noreply.github.com>
2023-12-24 12:43:45 +0300

    fix:unutulan route eklemesi

--- dev/src/routes/tasks.router.ts
+++ dev/src/routes/tasks.router.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
 import { addTask, doneTask } from '../controllers/tasks.controller';
+import { authorizationMiddleware } from '../middlewares/authorization.middleware';
 
 const router = express.Router();
 
-router.post('/add', addTask);
+router.post('/add',authorizationMiddleware(['creator']),addTask);
 router.post('/done', doneTask);
 
 export default router;
